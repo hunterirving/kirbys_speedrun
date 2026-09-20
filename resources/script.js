9242d5b2670074d7f01b61efbf638aacf3a0a682
Hunter <hunter@hunterirving.com>
2026-09-20 14:54:13 -0400

    fix glitched dedede sprite on end screen

--- resources/script.js
+++ resources/script.js
@@ -1,5 +1,5 @@
 const EXPECTED_IN = "90979baa1d0e24b41b5c304c5ddaf77450692d5a";
-const EXPECTED_OUT = "aa005616ba1cf0a20eb3fa431c02c32b6f288ea2";
+const EXPECTED_OUT = "6db16339ecfa3284da73308bdda068b2c5bce9d8";
 const FILENAME = "Kirby's Speedrun.gb";
 const JAPANESE = ["1e34b7beee30e350087771b3a3e05a40e4a1ea84", "5fe35fab25299b6c53b40decfe2b1827b4a64d2a"];
 const JAPANESE_HINT = "This patch is only compatible with the US/Europe release of the game.";
